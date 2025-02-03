2f737003b36dd34555c6be831860516ccf23cab2
nogy21 <nogy21@gmail.com>
2025-02-04 00:20:29 +0900

    🔧: MSW 설정 및 핸들러 구현
    - MSW 초기 세팅 추가
    - API 응답에 대한 MSW 핸들러 작성
    - main.tsx에서 코드블럭 내 await import문 사용을 위해 tsconfig module 설정 업그레이드

--- src/__mocks__/handlers.ts
+++ src/__mocks__/handlers.ts
@@ -1,16 +1,56 @@
 import { http, HttpResponse } from 'msw';
 
-import { Event } from '../types';
-import { events } from './response/events.json' assert { type: 'json' };
+import type { Event } from '../types';
+import { events } from './response/events.json';
+
+let mockEvents = {
+  events,
+};
 
 // ! HARD
 // ! 각 응답에 대한 MSW 핸들러를 작성해주세요. GET 요청은 이미 작성되어 있는 events json을 활용해주세요.
 export const handlers = [
-  http.get('/api/events', () => {}),
+  // events.json에 담긴 이벤트를 반환
+  http.get('/api/events', () => HttpResponse.json(mockEvents)),
 
-  http.post('/api/events', async ({ request }) => {}),
+  // request에 담긴 데이터를 받아와서 새로운 이벤트를 추가
+  http.post('/api/events', async ({ request }) => {
+    const data = (await request.json()) as Event;
+    const newEvent = { ...data, id: crypto.randomUUID() };
 
-  http.put('/api/events/:id', async () => {}),
+    mockEvents = {
+      events: [...mockEvents.events, newEvent],
+    };
 
-  http.delete('/api/events/:id', ({ params }) => {}),
+    return HttpResponse.json(newEvent, { status: 201 });
+  }),
+
+  // request에 담긴 데이터를 받아와서 기존 이벤트를 수정
+  http.put('/api/events/:id', async ({ request, params }) => {
+    const { id } = params;
+    const eventData = (await request.json()) as Event;
+
+    const targetEvent = mockEvents.events.find((event) => event.id === id);
+
+    if (!targetEvent) {
+      return HttpResponse.json({ message: 'Event not found' }, { status: 404 });
+    }
+
+    const updatedEvent = {
+      ...targetEvent,
+      ...eventData,
+    };
+    mockEvents.events = mockEvents.events.map((event) => (event.id === id ? updatedEvent : event));
+
+    return HttpResponse.json(updatedEvent);
+  }),
+
+  // request에 담긴 데이터를 받아와서 기존 이벤트를 삭제
+  http.delete('/api/events/:id', ({ params }) => {
+    const { id } = params;
+
+    mockEvents.events = mockEvents.events.filter((event) => event.id !== id);
+
+    return HttpResponse.json(null, { status: 204 });
+  }),
 ];
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./__mocks__/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  const root = createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>,
+  );
+});
